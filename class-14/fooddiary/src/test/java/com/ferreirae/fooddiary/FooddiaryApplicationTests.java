ce7ea242fb72753952db9d4aeb450d0badf820f7
Michelle Ferreirae <mnfmnfm@gmail.com>
2020-01-23 17:39:51 -0800

    end of day 14

--- class-14/fooddiary/src/test/java/com/ferreirae/fooddiary/FooddiaryApplicationTests.java
+++ class-14/fooddiary/src/test/java/com/ferreirae/fooddiary/FooddiaryApplicationTests.java
@@ -1,13 +1,46 @@
 package com.ferreirae.fooddiary;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FooddiaryApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private FoodDiaryController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("<h1>Food Diary</h1>")));
+	}
+
+	@Test
+	public void requestToNonexistentRoute() throws Exception {
+		this.mockMvc.perform(get("/not/good"))
+				.andDo(print())
+				.andExpect(status().isNotFound());
 	}
 
 }
